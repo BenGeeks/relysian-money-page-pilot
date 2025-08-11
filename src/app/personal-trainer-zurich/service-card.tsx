70b19f87882b795f73a7af4f531e2c9f80e966b3
Ben Gican II <b3nhur77@gmail.com>
2025-08-12 05:18:40 +0800

    removing links

--- src/app/personal-trainer-zurich/service-card.tsx
+++ src/app/personal-trainer-zurich/service-card.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import { gaEvent } from "@/lib/ga";
-import Link from "next/link";
+import { Button } from "antd";
 
 interface Props {
   title: string;
   desc: string;
-  href: string;
 }
-const ServiceCard = ({ title, desc, href }: Props) => {
+const ServiceCard = ({ title, desc }: Props) => {
   return (
-    <Link
-      href={href}
+    <Button
       onClick={() =>
         gaEvent("click_service", {
           page: "personal-trainer-zurich",
@@ -23,7 +21,7 @@ const ServiceCard = ({ title, desc, href }: Props) => {
     >
       <div className="text-xl text-white/95 font-semibold">{title}</div>
       <p className="mt-2 text-white/80">{desc}</p>
-    </Link>
+    </Button>
   );
 };
 
